690322994d8c0fb135386fb958c29281f8844637
ammar-hayder <ammar.hayder@gmail.com>
2022-10-20 23:31:54 +0530

    Testing 0

--- server.compiled.js
+++ server.compiled.js
@@ -3,9 +3,9 @@
 var _express = _interopRequireDefault(require("express"));
 var _path = _interopRequireDefault(require("path"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-var PORT = process.env.HTTP_PORT || 8081;
+var PORT = process.env.HTTP_PORT || 5000;
 var app = (0, _express["default"])();
-app.use(_express["default"]["static"](_path["default"].join(__dirname, 'client', 'build')));
+// app.use(express.static(path.join(__dirname, 'client', 'build')));
 app.get('/', function (req, res) {
   res.send('just gonna send it');
 });
